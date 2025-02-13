8eca496e715f7ef91c95961592b69f5e77dfdb93
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-02-13 14:32:10 +0700

    menambahkan tombol Login

--- app/src/main/java/com/example/smt2/LoginActivity.kt
+++ app/src/main/java/com/example/smt2/LoginActivity.kt
@@ -1,6 +1,8 @@
 package com.example.smt2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.buttonLogin)
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
