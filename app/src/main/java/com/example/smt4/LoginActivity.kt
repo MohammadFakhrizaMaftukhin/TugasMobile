ac29b06f965adae0d488a5b6084c44f331ba84e7
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-02-26 23:48:36 +0700

    perubahan nama package dan deklarasi fungsi untuk halaman login dan main

---
+++ app/src/main/java/com/example/smt4/LoginActivity.kt
@@ -0,0 +1,65 @@
+package com.example.smt4
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val usernameInput = findViewById<EditText>(R.id.usernameInput)
+        val passwordInput = findViewById<EditText>(R.id.passwordInput)
+        val loginButton = findViewById<Button>(R.id.loginButton)
+
+        usernameInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = s.toString()
+                if (input.isNotEmpty()) {
+                    Log.d("LoginActivity", "Username: $input")
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        passwordInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                val input = s.toString()
+                if (input.isNotEmpty()) {
+                    Log.d("LoginActivity", "Password: $input")
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+        loginButton.setOnClickListener {
+            val fullEmail = usernameInput.text.toString()
+            val username = fullEmail.split("@").firstOrNull() ?: fullEmail
+            
+            val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("username", username)
+            startActivity(intent)
+        }
+    }
+}
--- app/src/main/java/com/example/smt4/MainActivity.kt
+++ app/src/main/java/com/example/smt4/MainActivity.kt
@@ -1,27 +1,35 @@
-package com.example.smt2
+package com.example.smt4
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_main)
+        
+        // Get username from intent
+        val username = intent.getStringExtra("username") ?: "User"
+        
+        // Display welcome message with username
+        val welcomeText = findViewById<TextView>(R.id.welcomeText)
+        welcomeText.text = getString(R.string.welcome_message, username)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        val button = findViewById<Button>(R.id.buttonLogin)
+        val button = findViewById<Button>(R.id.buttonKembali)
         button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
