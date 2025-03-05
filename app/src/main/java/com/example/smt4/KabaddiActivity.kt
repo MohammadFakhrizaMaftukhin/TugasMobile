ffdb148316424a9d5c7bc54f5cb2df8ce4d701df
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-03-05 23:12:12 +0700

    menginisialisasi variabel skor dan komponen tampilan skor

--- app/src/main/java/com/example/smt4/KabaddiActivity.kt
+++ app/src/main/java/com/example/smt4/KabaddiActivity.kt
@@ -1,12 +1,18 @@
 package com.example.smt4
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class KabaddiActivity : AppCompatActivity() {
+    private var scoreTeamA = 0
+    private var scoreTeamB = 0
+    private lateinit var scoreTextA: TextView
+    private lateinit var scoreTextB: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
