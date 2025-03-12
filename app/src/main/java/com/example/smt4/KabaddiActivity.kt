f24368b6df3950d054bec88c3f36e0b0c42e543f
MohammadFakhrizaMaftukhin <fakhriza18b@gmail.com>
2025-03-12 21:49:58 +0700

    implementasi databinding dan livedata

--- app/src/main/java/com/example/smt4/KabaddiActivity.kt
+++ app/src/main/java/com/example/smt4/KabaddiActivity.kt
@@ -8,48 +8,60 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import com.example.smt4.databinding.ActivityKabaddiBinding
 
 class KabaddiActivity : AppCompatActivity() {
-    private lateinit var scoreTextA: TextView
-    private lateinit var scoreTextB: TextView
+//    private lateinit var scoreTextA: TextView
+//    private lateinit var scoreTextB: TextView
     private val viewModel: ScoreViewModel by viewModels()
+//    private lateinit var binding: ActivityKabaddiBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_kabaddi)
-        
+//        setContentView(R.layout.activity_kabaddi)
+
+        val binding : ActivityKabaddiBinding = DataBindingUtil.setContentView(this, R.layout.activity_kabaddi)
+
         // Initialize TextViews
-        scoreTextA = findViewById(R.id.textscoreA)
-        scoreTextB = findViewById(R.id.textscoreB)
+//        scoreTextA = findViewById(R.id.textscoreA)
+//        scoreTextB = findViewById(R.id.textscoreB)
 
         // Observe score changes
-        viewModel.scoreTeamA.observe(this) { score ->
-            scoreTextA.text = score.toString()
-        }
-        viewModel.scoreTeamB.observe(this) { score ->
-            scoreTextB.text = score.toString()
-        }
+//        val observerScoreA = Observer<Int>{ skorBaru ->
+//            binding.textscoreA.text = skorBaru.toString()
+//        }
+//        viewModel.scoreTeamA.observe(this@KabaddiActivity, observerScoreA)
+//        val observerScoreB = Observer<Int>{ skorBaru ->
+//            binding.textscoreB.text = skorBaru.toString()
+//        }
+//        viewModel.scoreTeamB.observe(this@KabaddiActivity, observerScoreB)
+
+        binding.scoreViewModel = viewModel
+        binding.lifecycleOwner = this
 
         // Team A buttons
-        findViewById<Button>(R.id.buttonplus1A).setOnClickListener {
+        binding.buttonplus1A.setOnClickListener {
             viewModel.addScoreTeamA(1)
         }
-        findViewById<Button>(R.id.buttonplus2A).setOnClickListener {
+        binding.buttonplus2A.setOnClickListener {
             viewModel.addScoreTeamA(2)
         }
 
         // Team B buttons
-        findViewById<Button>(R.id.buttonplus1B).setOnClickListener {
+        binding.buttonplus1B.setOnClickListener {
             viewModel.addScoreTeamB(1)
         }
-        findViewById<Button>(R.id.buttonplus2B).setOnClickListener {
+        binding.buttonplus2B.setOnClickListener {
             viewModel.addScoreTeamB(2)
         }
 
         // Reset button
-        findViewById<Button>(R.id.buttonreset).setOnClickListener {
+        binding.buttonreset.setOnClickListener {
             viewModel.resetScore()
+//            ScoreViewModel.scoreTeamA.removeObserver(observerScoreA)
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
